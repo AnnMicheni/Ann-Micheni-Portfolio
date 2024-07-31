d08a99f70074ef0292d01951104444f69b12f1bd
Manase <manasesmutembei11@gmail.com>
2024-07-31 17:04:33 +0300

    News component

---
+++ src/app/home/modals/message-modal/message-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-message-modal',
+  standalone: true,
+  imports: [ ],
+  templateUrl: './message-modal.component.html',
+  styleUrl: './message-modal.component.scss'
+})
+export class MessageModalComponent {
+
+}
