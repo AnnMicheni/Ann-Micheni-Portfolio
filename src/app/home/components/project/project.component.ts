d52af652d3724d8c4ca5c3df88a2c36b4013610b
Ann Micheni <60700848+AnnMicheni@users.noreply.github.com>
2024-09-10 18:02:07 +0300

    Update project.component.ts

--- src/app/home/components/project/project.component.ts
+++ src/app/home/components/project/project.component.ts
@@ -18,17 +18,17 @@ export class ProjectComponent {
     {
       title: 'Hospital Laboratory Information Systems',
       description: 'These systems are built to enhance the efficiency and accuracy of laboratory operations in a hospital setting. It streamlines the process of managing patient test results, tracking samples, and coordinating with healthcare providers. Features include real-time data entry, automated report generation, and integration with electronic health records (EHR), aiming to improve workflow, reduce errors, and provide timely insights for better patient care.',
-      image: 'hosp logo.png'
+      image: '/hosp logo.png'
     },
     {
       title: 'Pharmaciteucal Laboratory Information,
       description: 'It is a system built to optimize the management and analysis of data within pharmaceutical laboratories. The system supports tasks such as tracking experiments, managing inventory, and ensuring regulatory compliance. Key features include automated data collection, detailed reporting, and integration with other research tools. The goal is to enhance operational efficiency, ensure data integrity, and accelerate the research and development process in pharmaceutical labs.',
-      image: 'Pharma.png'
+      image: '/Pharma.png'
     },
     {
       title: 'Agricultural Research Laboratory Information System,
       description: 'It is a system designed to streamline and manage data for agricultural research laboratories. The system facilitates the organization of research data, tracking of experiments, and management of samples. Features include real-time data entry, detailed analytics, and integration with field data collection tools. The objective is to enhance research efficiency, ensure accurate data management, and support informed decision-making in agricultural studies.',
-      image: 'Agri1.jpg'
+      image: '/Agri1.jpg'
     },
   
   ];
