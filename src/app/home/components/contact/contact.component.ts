98585fef82118eebf73d08dd857f3fe4e7b02b16
Ann Micheni <60700848+AnnMicheni@users.noreply.github.com>
2024-09-10 17:05:40 +0300

    Update contact.component.ts

--- src/app/home/components/contact/contact.component.ts
+++ src/app/home/components/contact/contact.component.ts
@@ -11,32 +11,32 @@ import { CommonModule } from '@angular/common';
 export class ContactComponent {
   contacts = [
     {
-      link: 'https://x.com/G9teru',
+      link: 'https://x.com/anne_micheni',
       image: '/twitter.png',
       name: 'Twitter',
       message: 'Find me on X'
 
     },
     {
-      link: 'https://www.linkedin.com/in/manases-mutembei-49b011201',
+      link: 'https://www.linkedin.com/in/ann-micheni-6a3a73191/',
       image: '/link.png',
       name: 'LinkedIn',
       message: 'Follow me on LinkedIn'
     },
     {
-      link: 'https://github.com/manasesmutembei11',
+      link: 'https://github.com/AnnMicheni',
       image: '/git.jpeg',
       name: 'Github',
       message: 'Follow me on Github'
     },
     {
-      link: 'https://wa.me/+254768802661',
+      link: 'https://wa.me/+254720542537',
       image: '/whatsap.png',
       name: 'Whatsapp',
       message: 'Lets chat on whatsapp'
     },
     {
-      link: 'mailto:manasesmutembei11@gmail.com',
+      link: 'mailto:annemicheni5@gmail.com',
       image: '/gmail.png',
       name: 'Mail',
       message: 'Send an email'
